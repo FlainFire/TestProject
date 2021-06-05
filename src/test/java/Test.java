f70763cc08776308dba3eeafd673ac05984133de
Invilius <ynikbro1@gmail.com>
2021-06-05 13:42:43 +0300

    new commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,24 +4,74 @@ import com.codeborne.selenide.SelenideElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
-import java.util.List;
+import java.util.*;
 
-import static org.apache.commons.io.IOUtils.close;
+import static com.codeborne.selenide.Selenide.close;
 
+@SuppressWarnings("deprecation")
 public class Test {
+    public class InfoCollect {
+
+        private String contactName;
+        private List<String> contactEmails;
+
+
+        public InfoCollect(String contactName, List<String> contactEmails) {
+            this.contactName = contactName;
+            this.contactEmails = contactEmails;
+        }
+
+        public InfoCollect() {
+            this.contactName = "";
+            this.contactEmails = new LinkedList<>();
+        }
+
+    }
+
     @BeforeClass(alwaysRun = true)
     public void setUp() {
         Configuration.browser = "chrome";
     }
 
+    List<SelenideElement> contactEmailsList = Selenide.
+            $$x("//div[2]//details");  /**This one is more general and matches
+                                                       the number of contactNamesList in DOM (158)*/
+    List<SelenideElement> contactNamesList = Selenide.
+            $$x("//div[2]//details//summary/child::text()");
+    //List<String> stringNamesList = new LinkedList<>();
+
+    //div[@class='editor-content'][2]//div[2]//details//p[last()]//a[@href][last()]
+    /** The most precise contactEmailsList xpath.*/
+
+
     public static void main(String[] args) {
+        Test test = new Test();
         Selenide.open("https://www.gov.pl/web/poland-businessharbour-ru/itspecialist");
-        List<SelenideElement> contacts = Selenide.
-                $$x("//div[@class='editor-content'][2]//div[2]//details//p[last()]//a[@href][last()]");
-        for (SelenideElement cntcts : contacts) {
-            String strLinkText = cntcts.getOwnText();
-            System.out.println("Контакты: " + strLinkText);
+
+    }
+
+    /** I thought about this kind of logic to store data of each object in its List field
+     *
+     for (SelenideElement el : test.detailsList) {
+     if (el.$x(".//a[@href][last()]").sibling(0).exists()) {
+        some code to add it to the List<String> contactEmails of each object
+     }
+     */
+
+
+    private InfoCollect infoCollectInitializer(List<SelenideElement> contactNamesList, List<SelenideElement> contactEmailsList) {
+        InfoCollect result = new InfoCollect();
+
+
+        return result;
+    }
+
+    public List<InfoCollect> InfoCollectList(List<SelenideElement> contactNamesList, List<SelenideElement> contactEmailsList) {
+        List<InfoCollect> result = new LinkedList<>();
+        for (int i = 0; i <= contactNamesList.size(); i++) {
+            result.add(infoCollectInitializer(contactNamesList, contactEmailsList));
         }
+        return result;
     }
 
 
@@ -31,3 +81,4 @@ public class Test {
     }
 
 }
+
